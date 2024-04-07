41e40dbb60fe68ad45ba5a4d5029cbd38ea7cb99
code_otaku <academy3746@gmail.com>
2024-04-07 22:40:50 +0900

    :art: 공통 Formatting 파일 클래스화

--- lib/common/constant/date.dart
+++ lib/common/constant/date.dart
@@ -1,7 +1,9 @@
 import 'package:intl/intl.dart';
 
-/// 날짜 형식 Formatting
-final dateF = DateFormat('yyyy/MM/dd');
+class FormatUnits {
+  /// 날짜 형식 Formatting
+  static final date = DateFormat('yyyy/MM/dd');
 
-/// 시간 형식 Formatting
-final timeF = DateFormat('HH:mm');
+  /// 시간 형식 Formatting
+  static final time = DateFormat('HH:mm');
+}
