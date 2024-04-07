d9fc34730138bc73f026efbc01839f4e663424a9
code_otaku <academy3746@gmail.com>
2024-04-07 21:01:12 +0900

    :zap: 뒤로가기 종료 로직 성능 개선

--- lib/features/main/controllers/main_controller.dart
+++ lib/features/main/controllers/main_controller.dart
@@ -1,5 +1,17 @@
 import 'package:get/get.dart';
+import 'package:newbee_talk_mk2/common/utils/back_handler_button.dart';
 
 class MainCont extends GetxController {
   static MainCont get to => Get.find<MainCont>();
+
+  final _backHandlerButton = BackHandlerButton();
+
+  BackHandlerButton? get backHandlerButton => _backHandlerButton;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    backHandlerButton;
+  }
 }
--- lib/features/main/views/main_screen.dart
+++ lib/features/main/views/main_screen.dart
@@ -1,36 +1,23 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
-import 'package:newbee_talk_mk2/common/utils/back_handler_button.dart';
 import 'package:newbee_talk_mk2/features/main/controllers/main_controller.dart';
 
-class MainScreen extends StatefulWidget {
+class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   static const String routeName = '/main';
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  final cont = MainCont.to;
-
-  BackHandlerButton? backHandlerButton;
-
-  @override
-  void initState() {
-    super.initState();
-
-    backHandlerButton = BackHandlerButton();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final cont = MainCont.to;
+
+    final back = cont.backHandlerButton;
+
     return WillPopScope(
-      onWillPop: () async {
-        if (backHandlerButton != null) {
-          return backHandlerButton!.onWillPop(context);
+      onWillPop: () {
+        if (back != null) {
+          return back.onWillPop(context);
         }
 
         return Future.value(false);
