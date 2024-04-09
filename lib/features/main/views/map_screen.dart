50f578e8edae8cdacd5a79d4b690b8c01c5b34ae
code_otaku <academy3746@gmail.com>
2024-04-10 01:20:14 +0900

    :art: Naver Map 관련 기능 분리 리팩토링

--- lib/features/main/views/map_screen.dart
+++ lib/features/main/views/map_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
-import 'package:newbee_talk_mk2/features/main/controllers/main_controller.dart';
 import 'package:newbee_talk_mk2/features/main/controllers/map_controller.dart';
 
 class MapScreen extends StatelessWidget {
