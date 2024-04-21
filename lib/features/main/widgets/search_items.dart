23a9c6ae8a04be827fd06ca15f9cba0917a91cdf
academy3746 <academy3746@gmail.com>
2024-04-21 22:25:26 +0900

    :zap: 검색 화면 상세 페이지 라우트 처리 추가

--- lib/features/main/widgets/search_items.dart
+++ lib/features/main/widgets/search_items.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:newbee_talk_mk2/app_router.dart';
 import 'package:newbee_talk_mk2/common/constant/date.dart';
 import 'package:newbee_talk_mk2/common/constant/gaps.dart';
 import 'package:newbee_talk_mk2/common/constant/sizes.dart';
 import 'package:newbee_talk_mk2/common/widgets/common_text.dart';
 import 'package:newbee_talk_mk2/features/favorite/models/favorite.dart';
 import 'package:newbee_talk_mk2/features/main/controllers/search_controller.dart';
+import 'package:newbee_talk_mk2/features/store/controllers/detail_controller.dart';
 import 'package:newbee_talk_mk2/features/store/models/store.dart';
 
 class SearchItems extends StatefulWidget {
@@ -65,62 +67,71 @@ class _SearchItemsState extends State<SearchItems> {
     FoodStoreModel model,
     List<FavoriteModel> items,
   ) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: const EdgeInsets.symmetric(
-        horizontal: Sizes.size16,
-        vertical: Sizes.size12,
-      ),
-      decoration: ShapeDecoration(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(Sizes.size4),
-          side: const BorderSide(
-            color: Colors.black87,
-            width: 2,
+    return GestureDetector(
+      onTap: () {
+        final detail = DetailCont.to;
+
+        detail.setStoreData(model);
+
+        AppRouter.detail().to();
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size16,
+          vertical: Sizes.size12,
+        ),
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(Sizes.size4),
+            side: const BorderSide(
+              color: Colors.black87,
+              width: 2,
+            ),
           ),
         ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              CommonText(
-                textContent: model.storeName,
-                textColor: Colors.black87,
-                textSize: Sizes.size20,
-                textWeight: FontWeight.w700,
-              ),
-              _favoriteIcon(model, items),
-            ],
-          ),
-          Gaps.v12,
-          CommonText(
-            textContent: model.storeInfo,
-            textColor: Colors.black87,
-            textSize: Sizes.size14,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-          ),
-          Gaps.v20,
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              CommonText(
-                textContent: model.storeAddress,
-                textColor: Colors.black87,
-                textSize: Sizes.size12,
-              ),
-              CommonText(
-                textContent: date.format(model.createdAt!),
-                textSize: Sizes.size12,
-                textColor: Colors.black87,
-              ),
-            ],
-          ),
-        ],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CommonText(
+                  textContent: model.storeName,
+                  textColor: Colors.black87,
+                  textSize: Sizes.size20,
+                  textWeight: FontWeight.w700,
+                ),
+                _favoriteIcon(model, items),
+              ],
+            ),
+            Gaps.v12,
+            CommonText(
+              textContent: model.storeInfo,
+              textColor: Colors.black87,
+              textSize: Sizes.size14,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Gaps.v20,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CommonText(
+                  textContent: model.storeAddress,
+                  textColor: Colors.black87,
+                  textSize: Sizes.size12,
+                ),
+                CommonText(
+                  textContent: date.format(model.createdAt!),
+                  textSize: Sizes.size12,
+                  textColor: Colors.black87,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
